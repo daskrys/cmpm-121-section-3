bc0a219155ffbcade1105653ff95dcda226ab808
daskrys <caperezl@ucsc.edu>
2023-10-20 17:00:35 -0700

    second commit

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -3,7 +3,6 @@ import * as Phaser from "phaser";
 import starfieldUrl from "/assets/starfield.png";
 
 export default class Play extends Phaser.Scene {
-
   fire?: Phaser.Input.Keyboard.Key;
   left?: Phaser.Input.Keyboard.Key;
   right?: Phaser.Input.Keyboard.Key;
@@ -12,11 +11,14 @@ export default class Play extends Phaser.Scene {
   s?: Phaser.Input.Keyboard.Key;
   d?: Phaser.Input.Keyboard.Key;
 
+  enemies?: Phaser.GameObjects.Shape;
   starfield?: Phaser.GameObjects.TileSprite;
   spinner?: Phaser.GameObjects.Sprite;
-  //spinner?: Phaser.GameObjects.Shape;
-
-  rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
+  
+  moveBY: number = 5;
+  canMove: boolean = true;
+  rotationSpeed: number = Phaser.Math.PI2 / 1000; // radians per millisecond
+   //spinner?: Phaser.GameObjects.Shape;
 
   constructor() {
     super("play");
@@ -24,7 +26,7 @@ export default class Play extends Phaser.Scene {
 
   preload() {
     this.load.image("starfield", starfieldUrl);
-    this.load.image("dog", '/yoink.png')
+    this.load.image("dog", "/yoink.png");
   }
 
   #addKey(
@@ -52,12 +54,38 @@ export default class Play extends Phaser.Scene {
       )
       .setOrigin(0, 0);
 
-      this.spinner = this.add.sprite(50, 50, "dog").setScale(0.15);
+    this.spinner = this.add.sprite(50, 420, "dog").setScale(0.1);
+    this.enemies = this.add.rectangle(100, 100, 50, 50,  0x40fc1b );
     //this.spinner = this.add.rectangle(100, 100, 50, 50, 0x5548d2);
   }
 
+  movement () {
+  
+  /*  if (this.w!.isDown) {
+      this.spinner!.y -= this.moveBY;
+    } 
+  */
+    if (this.a!.isDown) {
+      this.spinner!.x -= this.moveBY;
+    }
+    /*
+    if (this.s!.isDown) {
+      this.spinner!.y += this.moveBY;
+    }
+    */
+    if (this.d!.isDown) {
+      this.spinner!.x += this.moveBY;
+    }
+    
+  }
+
+  respawn () {
+    //this.spinner!.x = 50;
+    //this.spinner!.y = 450;
+  }
+
   update(_timeMs: number, delta: number) {
-    this.starfield!.tilePositionX -= 4;
+    this.starfield!.tilePositionX -= this.moveBY;
 
     if (this.left!.isDown) {
       this.spinner!.rotation -= delta * this.rotationSpeed;
@@ -65,31 +93,25 @@ export default class Play extends Phaser.Scene {
     if (this.right!.isDown) {
       this.spinner!.rotation += delta * this.rotationSpeed;
     }
-
-    if(this.w!.isDown) {
-      this.spinner!.y -= 5;
-    }
-
-    if(this.a!.isDown) {
-      this.spinner!.x -= 5;
-    }
-
-    if(this.s!.isDown) {
-      this.spinner!.y += 5;
-    }
-
-    if(this.d!.isDown) {
-      this.spinner!.x += 5;
-    }
     
+    if(this.canMove)
+    {
+      this.movement();
+    }
 
     if (this.fire!.isDown) {
+      this.canMove = false;  
+
       this.tweens.add({
         targets: this.spinner,
-        scale: { from: 1.5, to: 1 },
-        duration: 300,
+        scale: { from: .10, to: .125 },
+        y: {from: this.spinner?.y, to: -50},
+        duration: 400,
         ease: Phaser.Math.Easing.Sine.Out,
       });
+
+      this.time.delayedCall(450, this.respawn);
+    
     }
   }
 }
